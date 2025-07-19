1664043a4f73c639e651f2fc3898c8ecdce47f28
Achraf <achraf.dev.it@gmail.com>
2025-07-19 14:22:47 +0100

    adding nav overlay to about us page .

---
+++ scripts/about us.js
@@ -0,0 +1,13 @@
+let humburgerBtn = document.querySelector('.humburger-btn');
+let humburgerIcon = document.querySelector('.humburger-icon');
+let overlayClass = document.querySelector('.overlay');
+
+humburgerBtn.addEventListener('click',()=>{
+  if (overlayClass.classList.contains('full-opacity')){
+    overlayClass.classList.remove('full-opacity');
+    humburgerIcon.src = './assets/svgs/humburger icon.svg';
+  }else{
+    overlayClass.classList.add('full-opacity');
+    humburgerIcon.src = './assets/svgs/close icon.svg';
+  }
+});
